e602b1f95e4badc9a81df06b0bd474e491f7b150
wspane <wadetheriver@gmail.com>
2022-01-26 09:52:11 -0800

    Regex validators for name fields in registration

--- forms.py
+++ forms.py
@@ -2,12 +2,21 @@ from wtforms import Form, StringField, TextAreaField, PasswordField, validators,
 
 
 class RegistrationForm(Form):
-    first_name = StringField('First Name', [validators.Length(min=1, max=50)])
-    last_name = StringField('Last Name', [validators.Length(min=1, max=50)])
+    first_name = StringField('First Name',
+                             [validators.Length(min=1, max=50),
+                              validators.Regexp("^[0-9a-zA-Z \-'_]+$",
+                              message="First name must contain only letters, numbers and hyphens")
+                              ])
+    last_name = StringField('Last Name',
+                            [validators.Length(min=1, max=50),
+                             validators.Regexp("^[0-9a-zA-Z \-'_]+$",
+                             message="Last name must contain only letters, numbers and hyphens")
+                             ])
     username = StringField('Username',
                            [validators.Length(min=4, max=25),
                             validators.Regexp('^\w+$',
-                            message="Username must contain only letters numbers or underscore")])
+                                              message="Username must contain only letters numbers or underscore")
+                            ])
     email = EmailField('Email', [validators.Length(min=6, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
